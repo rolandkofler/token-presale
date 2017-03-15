bddf5d0b3ecf9b5d4316c001039e00bce11a16ab
Roland Kofler <roland.kofler@gmail.com>
2017-03-15 12:25:53 +0100

    removed truffle

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var ConvertLib = artifacts.require("./owned.sol");
-var SikobaPresale = artifacts.require("./SikobaPresale.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, SikobaPresale);
-  deployer.deploy(SikobaPresale, {from:web3.eth.accounts[0], value:web3.toWei('15', 'ether'), gas:1000000});
-};
